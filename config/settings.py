6f18e057841011b9f5e51c0f3b460eb856306ee2
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-11-29 10:29:31 +0600

    feat: created common app and model

--- config/settings.py
+++ config/settings.py
@@ -9,12 +9,16 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    #Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Local Apps
+    'engage.common'
 ]
 
 MIDDLEWARE = [
